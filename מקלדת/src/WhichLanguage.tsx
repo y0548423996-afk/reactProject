65428784ad94b8ad09554d8c415a72d008e8e707
Efrat Gado <efratgado2@gmail.com>
2026-04-09 20:46:54 +0300

    Componnet WhichLanguage

---
+++ מקלדת/src/WhichLanguage.tsx
@@ -0,0 +1,27 @@
+// 1. הגדרת ה"חוזה" (Props)
+interface LetterProps {
+  char: string;     // האות עצמה (למשל 'א' או 'A')
+  language: string; // השפה (למשל 'he' או 'en')
+  // בהמשך תוסיפי כאן גם onClick כדי שהלחיצה תעבוד
+}
+
+// 2. הקומפוננטה
+function WhichLanguage({ char, language }: LetterProps) {
+
+  // כאן את יכולה להוסיף עיצוב שונה לפי השפה אם תרצי
+const isHebrew = language === 'he';
+const isEnglish = language === 'en';
+
+  return (
+  function LetterComponent({ char, language }: LetterProps) {
+  return (
+    <button className={`key ${language}`}> 
+      {/* כאן האות מוצגת. אם ב-char יש 'א', יופיע 'א' */}
+      {char}
+    </button>
+  );
+}
+  );
+}
+
+export default WhichLanguage;
